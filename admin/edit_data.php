8065ca2535c2668d0f76260f36c739d1aed84541
gloria <sugarcomb08@gmail.com>
2024-05-13 14:26:04 +0700

    add file

--- admin/edit_data.php
+++ admin/edit_data.php
@@ -50,7 +50,7 @@ if(isset($_POST["submit"])) {
         $unit = $_POST['unit'];
 
         // Unggah gambar
-        $target_dir = "../uploads/";
+        $target_dir = "../uploads";
         $target_file = $target_dir . basename($_FILES["image"]["name"]);
         move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
         $image = $target_file;
